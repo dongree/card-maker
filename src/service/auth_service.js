b62ae46648490c678a9069b931c1ba106c766ed4
dongree <dongwoo0307@naver.com>
2021-11-08 15:32:07 +0900

    add login component

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,13 +1,31 @@
-import { getAuth, signInWithPopup } from 'firebase/auth';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from 'firebase/auth';
 
 class AuthService {
   constructor() {
     this.firebaseAuth = getAuth();
+    this.googleProvider = new GoogleAuthProvider();
+    this.githubProvider = new GithubAuthProvider();
   }
 
   login(providerName) {
-    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    const authProvider = this.getProvider(providerName);
     return signInWithPopup(this.firebaseAuth, authProvider);
+  }
+
+  getProvider(providerName) {
+    switch (providerName) {
+      case 'Google':
+        return this.googleProvider;
+      case 'Github':
+        return this.githubProvider;
+      default:
+        throw new Error(`not supported provider: ${providerName}`);
+    }
   }
 }
 
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APP_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
