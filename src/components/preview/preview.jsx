8b96479d616e5a9009c2e75c6d13f38867d34dcc
dongree <dongwoo0307@naver.com>
2021-11-26 22:33:36 +0900

    remove unnecessary tag

--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -5,11 +5,9 @@ import styles from './preview.module.css';
 const Preview = ({ cards }) => (
   <section className={styles.preview}>
     <h1 className={styles.title}>Card Preview</h1>
-    <ul className={styles.cards}>
-      {Object.keys(cards).map(key => (
-        <Card key={key} card={cards[key]} />
-      ))}
-    </ul>
+    {Object.keys(cards).map(key => (
+      <Card key={key} card={cards[key]} />
+    ))}
   </section>
 );
 
