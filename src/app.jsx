f00b82ea4bc5c58e22b74a048c72498cba36215f
dongree <dongwoo0307@naver.com>
2021-11-28 11:50:09 +0900

    use helmet-aysnc

--- src/app.jsx
+++ src/app.jsx
@@ -3,22 +3,39 @@ import React from 'react';
 import styles from './app.module.css';
 import Login from './components/login/login';
 import Maker from './components/maker/maker';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const App = ({ FileInput, authService, cardRepository }) => {
   return (
     <div className={styles.app}>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/">
-            <Login authService={authService} />
-          </Route>
-          <Route path="/maker">
-            <Maker
-              FileInput={FileInput}
-              authService={authService}
-              cardRepository={cardRepository}
-            />
-          </Route>
+          <HelmetProvider>
+            <Route exact path="/">
+              <Helmet>
+                <title>Login</title>
+                <meta
+                  http-equiv="Content-Security-Policy"
+                  content="upgrade-insecure-requests"
+                ></meta>
+              </Helmet>
+              <Login authService={authService} />
+            </Route>
+            <Route path="/maker">
+              <Helmet>
+                <title>EPL Card Maker</title>
+                <meta
+                  http-equiv="Content-Security-Policy"
+                  content="upgrade-insecure-requests"
+                ></meta>
+              </Helmet>
+              <Maker
+                FileInput={FileInput}
+                authService={authService}
+                cardRepository={cardRepository}
+              />
+            </Route>
+          </HelmetProvider>
         </Switch>
       </BrowserRouter>
     </div>
